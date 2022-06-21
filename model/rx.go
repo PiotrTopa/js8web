2d26e4688dd28eb0f2f9c35db3168989b41a1458
piotr <piotr@pavilion.inward.icu>
2022-06-22 00:28:58 +0200

    RX_PACKETS db, model

--- model/rx.go
+++ model/rx.go
@@ -1,11 +1,25 @@
 package model
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
-var MODE_JS8 = "js8"
+var (
+	MODE_JS8                  = "js8"
+	EVENT_TYPE_RX_ACTIVITY    = "RX.ACTIVITY"
+	EVENT_TYPE_RX_DIRECTED    = "RX.DIRECTED"
+	EVENT_TYPE_RX_DIRECTED_ME = "RX.DIRECTED.ME:"
+	EVENT_TYPE_RX_SPOT        = "RX.SPOT"
+	EVENT_TYPE_RIG_PTT        = "RIG.PTT"
+	EVENT_TYPE_TX_FRAME       = "TX.FRAME"
+
+	SQL_RX_PACKETS_UPDATE = "UPDATE `RX_MESSAGES` SET `TYPE`=?, `CHANNEL`=?, `DIAL`=?, `FREQ`=?, `OFFSET`=?, `SNR`=?, `MODE`=?, `TIME_DRIFT`=?, `GRID`=?, `FROM`=?, `TO`=?, `TEXT`=?, `COMMAND`=?, `EXTRA`=? WHERE `ID`=?"
+	SQL_RX_PACKETS_INSERT = "INSERT INTO `RX_MESSAGES` (`TYPE`, `CHANNEL`, `DIAL`, `FREQ`, `OFFSET`, `SNR`, `MODE`, `TIME_DRIFT`, `GRID`, `FROM`, `TO`, `TEXT`, `COMMAND`, `EXTRA`) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+)
 
 type RxPacket struct {
-	Id        int
+	Id        int64
 	Type      string
 	Dial      uint32
 	Channel   uint16
@@ -35,6 +49,10 @@ func speedName(speed int) string {
 }
 
 func (o *RxPacket) Parse(event *Js8callEvent) error {
+	if event.Type != EVENT_TYPE_RX_ACTIVITY && event.Type != EVENT_TYPE_RX_DIRECTED && event.Type != EVENT_TYPE_RX_DIRECTED_ME {
+		return errors.New("Wrong event type, cannot parse params")
+	}
+
 	o.Type = event.Type
 	o.Dial = event.Params.Dial
 	o.Channel = uint16(event.Params.Offset / 50)
@@ -43,20 +61,43 @@ func (o *RxPacket) Parse(event *Js8callEvent) error {
 	o.Snr = event.Params.Snr
 	o.Mode = MODE_JS8
 	o.Speed = speedName(event.Params.Speed)
+	o.TimeDrift = uint16(1000 * event.Params.TimeDrift)
+	o.Grid = event.Params.Grid
+	o.From = event.Params.From
+	o.To = event.Params.To
+	o.Text = event.Params.Text
+	o.Command = event.Params.Command
+	o.Extra = event.Params.Extra
 	return nil
 }
 
 func (obj *RxPacket) Insert(db *sql.DB) error {
-	stmt, err := db.Prepare("INSERT INTO `RX`(`NAME`, `PASSWORD`, `ROLE`, `BIO`) values(?, ?, ?, ?)")
+	stmt, err := db.Prepare(SQL_RX_PACKETS_INSERT)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
-
-	res, err = stmt.Exec(&obj.Name, &obj.Password, &obj.Role, &obj.Bio)
+	//(TIME_DRIFT`, `GRID`, `FROM`, `TO`, `TEXT`, `COMMAND`, `EXTRA`)
+	res, err := stmt.Exec(
+		&obj.Type,
+		&obj.Channel,
+		&obj.Dial,
+		&obj.Freq,
+		&obj.Offset,
+		&obj.Snr,
+		&obj.Mode,
+		&obj.TimeDrift,
+		&obj.Grid,
+		&obj.From,
+		&obj.To,
+		&obj.Text,
+		&obj.Command,
+		&obj.Extra,
+	)
 	if err != nil {
 		return err
 	}
+	obj.Id, _ = res.LastInsertId()
 
 	return nil
 }
