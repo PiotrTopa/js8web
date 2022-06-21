f7acf6298b761d4180db9cad8d9ff8437a895683
piotr <piotr@basement.inward.icu>
2022-06-21 17:00:14 +0200

    Event types, parsing

--- event.go
+++ event.go
@@ -4,5 +4,22 @@ type Js8callEvent struct {
 	Type   string                 `json:"type"`
 	Value  string                 `json:"value"`
 	Params map[string]interface{} `json:"params"`
-	Test   int
+}
+
+type Event interface {
+}
+
+type EventRxActivity struct {
+	Type    string
+	Dial    uint32
+	Freq    uint32
+	Grid    string
+	From    string
+	To      string
+	Offset  uint16
+	Snr     int16
+	Speed   int
+	Command string
+	Text    string
+	Extra   string
 }
